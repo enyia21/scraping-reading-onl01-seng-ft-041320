ac251b907298a3a879b802859ef98974dbd273a0
Chibuisi Enyia <enyia21@gmail.com>
2020-05-04 11:40:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,10 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 # puts doc.css(".headline-26OIBN").text
 
-puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text
-# courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
-#
+# puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+
 # courses.each do |course|
 #   puts course.text.strip
 # end
