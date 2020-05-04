46304ff66980068a4152e76e2e6eb42059ac158a
Chibuisi Enyia <enyia21@gmail.com>
2020-05-04 11:41:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ doc = Nokogiri::HTML(html)
 # puts doc.css(".headline-26OIBN").text
 
 # puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC").text
-puts doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+puts doc.css("#3c239880-48cf-5ce7-84d0-578546d87586.inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 
 # courses.each do |course|
