e0a8f86e035e9f0308c734d52627dc160a8327c2
Chibuisi Enyia <enyia21@gmail.com>
2020-05-04 12:03:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,9 @@ doc = Nokogiri::HTML(html)
 # puts doc.css(".headline-26OIBN").text
 # puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 # puts doc.css("#3c239880-48cf-5ce7-84d0-578546d87586")
-puts doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 # puts doc.css(".title-oE5vT4")
-# courses.each do |course|
-#   puts course.text.strip
-# end
+courses.each do |course|
+  puts course.text.strip
+end
